318c759d5e080eb9dd2a2dd884ed5e03265456a1
JonCodesThings <jon@joncodesthings.net>
2020-03-01 17:17:29 +0000

    Escape characters kinda work now

--- src/AST/Constant/ASTConstantString.cpp
+++ src/AST/Constant/ASTConstantString.cpp
@@ -1,6 +1,46 @@
 #include <include/AST/Constant/ASTConstantString.hpp>
 
-ASTConstantString::ASTConstantString(const std::string &constant) : constant(constant.substr(1, constant.size() - 2)) {}
+const std::string PreProcess(std::string s)
+{
+    std::string n;
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        if ((*it) == '\\')
+        {
+            auto next = std::next(it);
+            if (next != s.end())
+            {
+                switch (*next)
+                {
+                    default:
+                        break;
+                    case 'n':
+                    {
+                        n.push_back('\n');
+                        it++;
+                        break;
+                    }
+                    case 't':
+                    {
+                        n.push_back('\t');
+                        it++;
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            n.push_back(*it);
+        }
+    }
+    return n;
+}
+
+ASTConstantString::ASTConstantString(const std::string &constant) : constant(PreProcess(constant.substr(1, constant.size() - 2))) 
+{
+    
+}
 
 llvm::Value *ASTConstantString::EmitIR(IREmitter::EmitterState &state) 
 { 
