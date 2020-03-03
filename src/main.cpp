6021fa2b17f2019a6a98417ba0c9056f6f3963a0
JonCodesThings <1600538@uad.ac.uk>
2020-03-03 11:34:00 +0000

    Type scanner no longer breaks everything

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,28 @@
 
 extern std::unique_ptr<ASTBlock> base;
 extern FILE *yyin;
+extern FILE *type_in;
 extern const char *yycurrentfilename;
+extern const char *type_currentfilename;
 extern int yyparse();
+extern int type_parse();
 extern TypeRegistry *registry;
 
 
 int main(int argc, const char **args)
 {
+    type_in = fopen(args[1], "r");
+    type_parse();
+    fclose(type_in);
+
     yyin = fopen(args[1], "r");
     if (!yyin)
         return 0;
     yycurrentfilename = args[1];
+    type_currentfilename = args[1];
     llvm::LLVMContext context;
     registry = new TypeRegistry(context);
     registry->SetupBuiltinJCTypes();
-    type_parse();
     int p = yyparse();
     if (base)
     {
