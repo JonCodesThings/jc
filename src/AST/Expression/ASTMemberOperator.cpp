3a1e8e76a653949ae7e7eb82c9997772907322e1
JonCodesThings <jon@joncodesthings.net>
2020-03-05 18:26:58 +0000

    Started working on member operator

--- src/AST/Expression/ASTMemberOperator.cpp
+++ src/AST/Expression/ASTMemberOperator.cpp
@@ -1,17 +1,33 @@
 #include <include/AST/Expression/ASTMemberOperator.hpp>
 
-ASTMemberOperator::ASTMemberOperator(ASTIdentifier &id, ASTIdentifier &member, OP op) : id(&id), member_id(&member), OP(op) {}
+ASTMemberOperator::ASTMemberOperator(ASTIdentifier &id, ASTIdentifier &member, OP op) : id(&id), member_id(&member), op(op), ASTExpression(MEMBER_OP) {}
 
 llvm::Value *ASTMemberOperator::EmitIR(IREmitter::EmitterState &state)
 {
+    const Symbol *struct_symbol = state.symbolStack.GetSymbolByIdentifier(id->identifier);
+    const JCType *struct_type = state.typeRegistry.GetTypeInfo(struct_symbol->type);
     switch (op)
     {
         default:
             return NULL;
+        case DOT:
+        {
+            for (int i = 0; i < struct_type->MEMBER_NAMES.size(); i++)
+            {
+                if (struct_type->MEMBER_NAMES[i] == member_id->identifier)
+                {
+                    llvm::Value *get_element = state.builder.CreateGEP(struct_symbol->alloc_inst, { llvm::ConstantInt::get(llvm::Type::getInt32Ty(state.context), 0),  llvm::ConstantInt::get(llvm::Type::getInt32Ty(state.context), i) });
+                    llvm::Value *load_element = state.builder.CreateLoad(get_element, "load_struct_element_" + member_id->identifier);
+                    return load_element;
+                }
+            }
+        }
     }
 }
 
 const std::string *ASTMemberOperator::GetType(IREmitter::EmitterState &state)
 {
-    
+    const Symbol *struct_symbol = state.symbolStack.GetSymbolByIdentifier(id->identifier);
+    const JCType *struct_type = state.typeRegistry.GetTypeInfo(struct_symbol->type);
+    return NULL;
 }
